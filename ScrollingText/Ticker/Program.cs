e4c3d4a629dad3c8369223a489682ac2192ad791
tl <mail@torsten-lindig.de>
2019-01-07 16:04:15 +0100

    c1

--- ScrollingText/Ticker/Program.cs
+++ ScrollingText/Ticker/Program.cs
@@ -10,9 +10,9 @@ namespace Ticker
             Console.Title = "Running Line";
             Console.ForegroundColor = ConsoleColor.Cyan;
             var rl = new RunningLineCreator();
-            rl.Advertisments.Add("Վաճառվում է բնակարան Ղափանի կենտրոնում, կոդ DP154, գինը՝ պայմանագրային");
-            rl.Advertisments.Add("Իջևանի կարատեի դպրոցն անց է կացնում մարզումներ 4 տարեկանից բարձր անձանց համար");
-            rl.Advertisments.Add("Այստեղ կարող է լինել Ձեր գովազդը");
+            rl.Advertisments.Add("In der Kantine gibt es heute das Essen vom Vortag zum halben Preis.");
+            rl.Advertisments.Add("Die Fahrradständer sind jetzt kostenpflichtig. Herr Adler hat diese gepachtet und bietet dafür Mietverträge an.");
+            rl.Advertisments.Add("--- hier könnte auch Ihre Anzeige stehen ---");
 
             rl.RunTheLine();
 
